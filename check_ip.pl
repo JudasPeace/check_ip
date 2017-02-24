13f4f6acb03e172d46429c1944ef9034128c0a61
JudasPeace <petelock@gmail.com>
2017-02-24 15:43:46 +0000

    Update check_ip.pl

--- check_ip.pl
+++ check_ip.pl
@@ -15,13 +15,4 @@ if ( $ip =~ /^($ipno(?:\.|$)){4}/ ){
     print "IP OK\n";
 };
 
-
-
 exit;
-
-__DATA__
-
-I went for an interview at Arm in Cambridge, UK. They asked me to write one on
-the board and I wrote some lame one .. and later ... brooding on my poor
-attempt set out to make a better one. Driven by failure? Or maybe just really
-irritated by it. I still got the job :)

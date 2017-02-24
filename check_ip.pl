5ad465be6dc3bf1b5723217a8818b9c404693bad
JudasPeace <petelock@gmail.com>
2017-02-24 15:56:56 +0000

    Update check_ip.pl

--- check_ip.pl
+++ check_ip.pl
@@ -1,7 +1,14 @@
 #!/usr/bin/perl -w
-#I like this one using quoted reg ex
 
-my $ip = '192.168.255.254'; # for example
+my $ip = '';
+if (defined $ARGV[0]){
+	$ip=$ARGV[0];
+}else{
+	print "\n\tUsage: check_ip.pl IP_ADDRESS\n";
+	print "\n\tExample perl check_ip.pl 192.168.0.1\n\n";
+	exit;
+}
+
 # Set up the quoted regex
 my $ipno = qr/
     2(?:5[0-5] | [0-4]\d)
@@ -12,7 +19,9 @@ my $ipno = qr/
 /x;
 # To test an IP address
 if ( $ip =~ /^($ipno(?:\.|$)){4}/ ){
-    print "IP OK\n";
-};
+    print "IP address OK\n";
+}else{
+	print "IP address not Valid\n";
+}
 
 exit;
